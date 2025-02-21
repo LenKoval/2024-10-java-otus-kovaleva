aecfa9ec05e4c4c78ef82b483870f0a24795006a
ElenaKovaleva <lenshemeta@gmail.com>
2025-02-21 11:55:23 +0300

    hw08 small fix

--- hw08-serialization/src/test/java/ru/otus/kovaleva/dataprocessor/ProcessorTest.java
+++ hw08-serialization/src/test/java/ru/otus/kovaleva/dataprocessor/ProcessorTest.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
