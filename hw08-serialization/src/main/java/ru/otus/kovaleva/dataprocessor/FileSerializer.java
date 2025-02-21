dbfa84019a8b01ab6ab65a196e552dbff07be259
ElenaKovaleva <lenshemeta@gmail.com>
2025-02-21 11:48:20 +0300

    hw08 done

--- hw08-serialization/src/main/java/ru/otus/kovaleva/dataprocessor/FileSerializer.java
+++ hw08-serialization/src/main/java/ru/otus/kovaleva/dataprocessor/FileSerializer.java
@@ -1,13 +1,41 @@
 package ru.otus.kovaleva.dataprocessor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
 
-    public FileSerializer(String fileName) {}
+    private final String fileName;
+
+    private final ObjectMapper objectMapper;
+
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+        this.objectMapper = new ObjectMapper();
+    }
 
     @Override
     public void serialize(Map<String, Double> data) {
-        // формирует результирующий json и сохраняет его в файл
+        try {
+
+            if (data == null || data.isEmpty()) {
+                throw new FileProcessException("Empty data for serialization");
+            }
+
+            Path path = Paths.get(fileName);
+            Files.createDirectories(path.getParent());
+
+            try (OutputStream outputStream = Files.newOutputStream(path)) {
+                objectMapper.writeValue(outputStream, data);
+            }
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
--- hw08-serialization/src/main/java/ru/otus/kovaleva/dataprocessor/ProcessorAggregator.java
+++ hw08-serialization/src/main/java/ru/otus/kovaleva/dataprocessor/ProcessorAggregator.java
@@ -2,15 +2,20 @@ package ru.otus.kovaleva.dataprocessor;
 
 import ru.otus.kovaleva.model.Measurement;
 
-import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ProcessorAggregator implements Processor {
 
     @Override
     public Map<String, Double> process(List<Measurement> data) {
-        // группирует выходящий список по name, при этом суммирует поля value
-        return Collections.emptyMap();
+        return data.stream()
+                .collect(Collectors.groupingBy(
+                        Measurement::name,
+                        LinkedHashMap::new,  // HashMap не гарантирует порядок поэтому использовала LinkedHasMap
+                        Collectors.summingDouble(Measurement::value)
+                ));
     }
 }
