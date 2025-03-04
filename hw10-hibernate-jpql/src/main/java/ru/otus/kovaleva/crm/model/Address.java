57ed9ba12c03a2ea033ed4e640e80153d4b99329
ElenaKovaleva <lenshemeta@gmail.com>
2025-03-04 15:39:06 +0300

    hw10 added teachers part

---
+++ hw10-hibernate-jpql/src/main/java/ru/otus/kovaleva/crm/model/Address.java
@@ -0,0 +1,8 @@
+package ru.otus.kovaleva.crm.model;
+
+public class Address {
+
+    public Address(Long id, String street) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ hw10-hibernate-jpql/src/main/java/ru/otus/kovaleva/crm/model/Phone.java
@@ -0,0 +1,8 @@
+package ru.otus.kovaleva.crm.model;
+
+public class Phone {
+
+    public Phone(Long id, String number) {
+        throw new UnsupportedOperationException();
+    }
+}
